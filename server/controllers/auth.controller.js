5a097e1f1ad1b02ce3bb0e91aa71775747f65878
AhmedRazaSidd <ar5812264@gmail.com>
2025-06-17 15:26:56 -0700

    Added: Redis config

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,44 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js'
+import { redis } from '../lib/redis.js'
+
+const generateTokens = (userId) => {
+
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '15m'
+    });
+
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: '7d'
+    });
+
+    return { accessToken, refreshToken }
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+    await redis.set(`refresh_token:${userId}`, refreshToken, 'EX', 7 * 24 * 60 * 60);
+}
+
 export const signup = async (req, res) => {
-    res.send('Sign Up route called');
+
+    const { email, password, name } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await User.create({ name, email, password });
+
+        // authentication
+        const { accessToken, refreshToken } = generateTokens(user._id)
+        await storeRefreshToken(user._id, refreshToken);
+        res.status(201).json({ user, message: 'User created successfully' });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 export const login = async (req, res) => {
     res.send('Login In route called');

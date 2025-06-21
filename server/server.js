01c2ecc3968819742b1e0b940aeb4f62df3da1bd
AhmedRazaSidd <ar5812264@gmail.com>
2025-06-21 15:18:07 -0700

    Added: Stripe Payment Method

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv/config";
-import authRoute from "./routes/auth.route.js";
-import productRoute from "./routes/product.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import productRoutes from "./routes/product.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.router.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -11,8 +14,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allows you to parse the body of the request
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
-app.use("/api/products", productRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);

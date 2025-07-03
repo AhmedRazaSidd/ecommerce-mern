76d3c442384e30f4c84b545fd2796283a9253b89
AhmedRazaSidd <ar5812264@gmail.com>
2025-07-03 15:24:09 -0700

    Added: Cors in Backend

--- server/src/server.js
+++ server/src/server.js
@@ -8,11 +8,18 @@ import paymentRoutes from "./routes/payment.route.js";
 import analyticRoutes from "./routes/analytic.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(
+  cors({
+    origin: "https://ecommerce-mern-ahmed.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);

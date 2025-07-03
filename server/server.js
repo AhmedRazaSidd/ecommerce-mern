59ae14ceabbb1bae94597c7ff9be461b7627ad2e
AhmedRazaSidd <ar5812264@gmail.com>
2025-07-03 14:47:45 -0700

    Project: Completed

--- server/server.js
+++ server/server.js
@@ -8,9 +8,12 @@ import paymentRoutes from "./routes/payment.route.js";
 import analyticRoutes from "./routes/analytic.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -21,6 +24,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+   app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
